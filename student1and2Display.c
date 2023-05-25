6459604298e9ded4ace5322cf98c384c3ca55bf0
Ronak-Razavi <132320940+Ronak-Razavi@users.noreply.github.com>
2023-05-25 09:29:03 -0400

    student 1 and student 2 display

---
+++ student1and2Display.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+# include <Stdlib.h>
+
+struct student
+  {
+      int id;
+      char * firstName;
+      char * lastName;
+      float gpa;
+
+  };
+
+int main()
+{
+  struct student stu1;
+  stu1.id=101;
+  stu1.firstName="Roy";
+  stu1.lastName="Green";
+  stu1.gpa=3.78;
+
+  struct student stu2={102,"Lili","Brown",3.25};
+  DisplayStudent (stu1);
+  DisplayStudent(stu2);
+    return 0;
+}
+void DisplayStudent (struct student stu)
+{
+    printf("student id:%d\n",stu.id );
+    printf("First Name:%s\n",stu.firstName);
+    printf("Last Name:%s\n",stu.lastName);
+    printf("GPA :%2f\n",stu.gpa);
+    printf("\n\n");
+
+
+}
